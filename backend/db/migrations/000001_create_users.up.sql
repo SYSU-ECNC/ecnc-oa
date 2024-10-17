ee6622889f7845b1e800c8a05d12159778407461
jonathanhu237 <jonathanhu237@gmail.com>
2024-10-17 15:18:55 +0800

    feat: do not use pgsql to generate uuid any more

--- backend/db/migrations/000001_create_users.up.sql
+++ backend/db/migrations/000001_create_users.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE
     IF NOT EXISTS users (
-        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        id UUID PRIMARY KEY,
         username TEXT NOT NULL UNIQUE,
         password_hash TEXT NOT NULL,
         full_name TEXT NOT NULL,
@@ -11,9 +11,10 @@ CREATE TABLE
     );
 
 INSERT INTO
-    users (username, password_hash, full_name, role)
+    users (id, username, password_hash, full_name, role)
 VALUES
     (
+        'f03cd9cc-9383-49b3-84f9-cfea75074e82',
         'admin',
         '$2a$10$EXiXrDu1nb1.Gu/gSnIPCu/aUWAaTCmNRtR8U.WP2JPc1bbfQCib2',
         '初始管理员',
