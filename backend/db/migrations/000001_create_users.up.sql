5b947f863d9e9316f2f763b454752349aaf3602d
jonathanhu237 <jonathanhu237@gmail.com>
2024-10-17 12:48:10 +0800

    feat: create users table

---
+++ backend/db/migrations/000001_create_users.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE
+    IF NOT EXISTS users (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        username TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL,
+        full_name TEXT NOT NULL,
+        role TEXT NOT NULL,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW ()
+    );
+
+INSERT INTO
+    users (username, password_hash, full_name, role)
+VALUES
+    (
+        'admin',
+        '$2a$10$EXiXrDu1nb1.Gu/gSnIPCu/aUWAaTCmNRtR8U.WP2JPc1bbfQCib2',
+        '初始管理员',
+        '黑心'
+    );
