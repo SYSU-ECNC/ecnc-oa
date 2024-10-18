38fd6e2a991826da65d68380bea3111c118d638a
jonathanhu237 <jonathanhu237@gmail.com>
2024-10-19 00:48:59 +0800

    fix: adjust the import path

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,22 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/styles/globals.css";
 import { ReactNode } from "react";
 import localFont from "next/font/local";
 
 const LXGWWenKai = localFont({
   src: [
     {
-      path: "./fonts/LXGWWenKai/LXGWWenKai-Light.ttf",
+      path: "../../public/fonts/LXGWWenKai/LXGWWenKai-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./fonts/LXGWWenKai/LXGWWenKai-Regular.ttf",
+      path: "../../public/fonts/LXGWWenKai/LXGWWenKai-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/LXGWWenKai/LXGWWenKai-Medium.ttf",
+      path: "../../public/fonts/LXGWWenKai/LXGWWenKai-Medium.ttf",
       weight: "500",
       style: "normal",
     },
@@ -26,6 +26,9 @@ const LXGWWenKai = localFont({
 export const metadata: Metadata = {
   title: "ECNC OA",
   description: "用于 ECNC 排班，考勤，请假的系统",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 interface RootLayoutProps {
@@ -34,8 +37,10 @@ interface RootLayoutProps {
 
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
-    <html lang="zh-CN">
-      <body className={`${LXGWWenKai.className} antialiased`}>{children}</body>
+    <html lang="zh-CN" className="m-0 h-screen">
+      <body className={`${LXGWWenKai.className} antialiased m-0 h-screen`}>
+        {children}
+      </body>
     </html>
   );
 };
